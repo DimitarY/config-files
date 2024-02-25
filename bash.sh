529605ae5765718cd36ff3da3b625e8ee42c3ac6
Dimitar Yordanov <52638215+DimitarY@users.noreply.github.com>
2024-02-25 22:20:43 +0200

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,7 @@
+# Enable continuous reverse history search with up arrow
+bind '"\e[A": history-search-backward'
+
+# Enable continuous reverse search forward with down arrow
+bind '"\e[B": history-search-forward'
+
+export EDITOR=vim
